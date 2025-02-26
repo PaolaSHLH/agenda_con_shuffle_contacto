d1f8c30a940e0011a1316e80d9538a2c1710ae26
Danna <you@example.com>
2025-02-26 09:47:35 -0700

    [inicializa] El programa de agenda

---
+++ Agenda_shuffle/Agenda_shuffle/Agenda_shuffleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Agenda_shuffleApp.swift
+//  Agenda_shuffle
+//
+//  Created by alumno on 26/2/25.
+//
+
+import SwiftUI
+
+@main
+struct Agenda_shuffleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
@@ -0,0 +1,18 @@
+//
+//  PantallaAgenda.swift
+//  Agenda_shuffle
+//
+//  Created by alumno on 26/2/25.
+//
+
+import SwiftUI
+
+struct PantallaAgenda: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PantallaAgenda()
+}
