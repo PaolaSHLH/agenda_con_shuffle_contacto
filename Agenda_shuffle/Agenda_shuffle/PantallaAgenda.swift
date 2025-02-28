e3798bc93f1e3229a8c6b4de74051a59e42a9b32
Danna <you@example.com>
2025-02-28 10:45:26 -0700

    [Modifica el previewde contacto]

--- Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
+++ Agenda_shuffle/Agenda_shuffle/PantallaAgenda.swift
@@ -10,24 +10,37 @@ import SwiftUI
 var contactos = [
     ContactoAgenda(nombre: "Chanyeol", telefono: "610497"),
     ContactoAgenda(nombre: "Sehun", telefono: "123576"),
-    ContactoAgenda(nombre: "Baekhyun", telefono: "049925")
+    ContactoAgenda(nombre: "Baekhyun", telefono: "049925"),
+    ContactoAgenda(nombre: "Suho", telefono: "610497"),
+    ContactoAgenda(nombre: "Kai", telefono: "123576"),
+    ContactoAgenda(nombre: "D.O", telefono: "049925"),
+    ContactoAgenda(nombre: "Lay", telefono: "610497"),
+    ContactoAgenda(nombre: "Xiumin", telefono: "123576"),
+    ContactoAgenda(nombre: "Chen", telefono: "049925")
+
+
 
     
 ]
 
 struct PantallaAgenda: View {
+    var largo_de_pantalla = UIScreen.main.bounds.width
+    var ancho_de_pantalla = UIScreen.main.bounds.height
+    
     var body: some View {
 
-        List{
-            VStack{
+        ScrollView{
+            VStack(spacing:10){
                 ForEach(contactos){ contacto in
-                    Text("\(contacto.nombre)")
+                    //Text("\(contacto.nombre)")
+                    ContactoPreview(contacto_a_mostrar: contacto, alPulsar: {print("te envia saludos \(contacto.nombre) desde la pantalla de agenda")})
                 }
             }
             .background(Color.mint)
+            .frame(width: largo_de_pantalla, alignment: Alignment.center)
+
         }
-        
-        
+        .background(Color.red)
     }
 }
 
--- Agenda_shuffle/Agenda_shuffle/componentes/contacto_preview.swift
+++ Agenda_shuffle/Agenda_shuffle/componentes/contacto_preview.swift
@@ -6,17 +6,43 @@
 //
 
 import SwiftUI
+let contacto_prueba = ContactoAgenda(nombre:"Chanyeol", telefono: "789654")
+
+let esquinasRedondeadas = CGSize(width: 55, height: 55)
 
 struct ContactoPreview: View {
+    var contacto_a_mostrar: ContactoAgenda
+    var alPulsar : () -> Void = {print("No se ha implementado")}
+    
     var body: some View {
-        HStack{
-            Image(systemName: "person.crop.circle.fill")
-            Text("Quiero un bolillo")
-            Text("My lovely")
+        HStack(alignment: VerticalAlignment.center){
+            Spacer()
+            VStack{
+                Image("imagen")
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                    .frame(width: 60, height: 60, alignment: .center)
+                    .clipShape(RoundedRectangle(cornerSize: /*@START_MENU_TOKEN@*/CGSize(width: 20, height: 10)/*@END_MENU_TOKEN@*/))
+            }//.background(Color.white)
+            
+            Spacer()
+            
+            VStack(alignment: HorizontalAlignment.leading){
+                Text(contacto_a_mostrar.nombre)
+                Text(contacto_a_mostrar.telefono)
+            }
+            .frame(width: 80)
+            
+            Spacer()
+        }
+        .frame(width: 250)
+        .background(Color.indigo)
+        .onTapGesture {
+            alPulsar()
         }
     }
 }
 
 #Preview {
-    ContactoPreview()
+    ContactoPreview(contacto_a_mostrar: contacto_prueba)
 }
